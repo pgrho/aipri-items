84963f70c57e9aa1a13b415528218cec8d73a80e
pgrho <shipwreck@live.jp>
2025-03-26 11:01:04 +0900

    Added Chapter.TryParseId

--- src/Shipwreck.Aipri/Chapter.cs
+++ src/Shipwreck.Aipri/Chapter.cs
@@ -1,6 +1,8 @@
-﻿namespace Shipwreck.Aipri;
+﻿using System.Text.RegularExpressions;
 
-public sealed class Chapter : DataItem
+namespace Shipwreck.Aipri;
+
+public sealed partial class Chapter : DataItem
 {
     public string Id { get; set; } = string.Empty;
     public string Name { get; set; } = string.Empty;
@@ -15,4 +17,39 @@ public sealed class Chapter : DataItem
             Start = Start,
             End = End,
         };
+
+    [GeneratedRegex("^(?:ring)?[1-9]$")]
+    private static partial Regex ChapterIdPattern();
+
+    public static long ParseIdOrder(string id)
+    {
+        if (TryParseId(id, out var y, out var n))
+        {
+            return y * 10 + n;
+        }
+        return int.MaxValue;
+    }
+
+    public static bool TryParseId(string id, out int year, out int no)
+    {
+        if (id != null)
+        {
+            var m = ChapterIdPattern().Match(id);
+            if (m.Success)
+            {
+                year = id[0] switch
+                {
+                    'r' or 'R' => 2,
+                    >= '1' and <= '9' => 1,
+                    _ => int.MaxValue
+                };
+                no = id[id.Length - 1] - '0';
+
+                return true;
+            }
+        }
+
+        year = no = int.MaxValue;
+        return false;
+    }
 }
